fd569089c0a846d920be4fa3c1cf1193aabf17e9
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-02-26 15:00:47 +0900

    Refactor : Lv1_3 validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,8 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+    @Pattern(regexp = "^[A-Z0-9]+$", message = "새 비밀번호는 숫자와 영어 대문자를 포함해야합니다")
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야합니다")
+    @NotBlank(message = "새 비밀번호는 필수 입력값입니다")
     private String newPassword;
 }
